08ce37f0b2a78e18e6bd2896edfacef33fb5d8ed
P-Kshitij <kshitijparwani.mat18@itbhu.ac.in>
2020-09-25 17:43:55 +0000

    added gitignore and setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+kaggle datasets download -d lakshmi25npathi/imdb-dataset-of-50k-movie-reviews -p input/
+kaggle datasets download -d abhishek/bert-base-uncased -p input/
+mkdir input/bert-base-uncased
+unzip input/bert-base-uncased.zip -d input/bert-base-uncased/
+rm input/bert-base-uncased.zip
+unzip input/imdb-dataset-of-50k-movie-reviews.zip -d input/
+rm input/imdb-dataset-of-50k-movie-reviews.zip
